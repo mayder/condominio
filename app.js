af426b177d8c3085008db50304a246046ccb9605
piterlink <piterlink@gmail.com>
2017-02-01 12:27:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
+console.log('Breno Gay');
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
